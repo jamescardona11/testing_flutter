53950df6bfae5c6dde74f6e28a3587d7887a2ef3
jamescardona11 <jjcardonao@gmail.com>
2020-08-19 11:22:31 -0500

    counter test from cookbook

--- test/unit testing.dart
+++ test/unit testing.dart
@@ -1,13 +1,39 @@
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
+import 'package:testing_flutter/cookbook/counter.dart';
 import 'package:testing_flutter/unittesting/post_helper.dart';
 import 'package:testing_flutter/unittesting/randomizer.dart';
 
 class MockPostHelper extends Mock implements PostHelper {}
 
 void main() {
-  randomizerTesting();
-  httpPostHelperTest();
+  //randomizerTesting();
+  //httpPostHelperTest();
+  counterTest();
+}
+
+void counterTest() {
+  group('Counter', () {
+    test('value should start at 0', () {
+      expect(Counter().value, 0);
+    });
+
+    test('value should be incremented', () {
+      final counter = Counter();
+
+      counter.increment();
+
+      expect(counter.value, 1);
+    });
+
+    test('value should be decremented', () {
+      final counter = Counter();
+
+      counter.decrement();
+
+      expect(counter.value, -1);
+    });
+  });
 }
 
 void httpPostHelperTest() {
